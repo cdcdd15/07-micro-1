df9347e33f17bae255c9815298d2c783a03db8a7
Cosmin Dinu <cdcdd15@gmail.com>
2021-05-11 14:58:07 +0300

    Changes made for zuul integration.

--- src/main/java/com/cosmin/feign/Micro1FeignClient.java
+++ src/main/java/com/cosmin/feign/Micro1FeignClient.java
@@ -2,14 +2,16 @@ package com.cosmin.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "07-micro-2")
+@FeignClient(name = "netflix-zuul-api-gateway-server")
+//@FeignClient(name = "07-micro-2")
 //@FeignClient(name = "micro1-service", url = "${SERVICE_URL:http://localhost:5001}")
 public interface Micro1FeignClient {
 	
-	@RequestMapping(value = "/hello-world", method = RequestMethod.GET)
+	@GetMapping(value = "/07-micro-2/hello-world")
+//	@RequestMapping(value = "/hello-world", method = RequestMethod.GET)
 	public ResponseEntity<String> getMessageFromMicro2();
 
 }
