051ce0306c945e45f67d2045d34b6c0fba78f580
Cosmin Dinu <cdcdd15@gmail.com>
2021-05-28 14:56:58 +0300

    Created script to build all projects (including their Docker images locally) and push images to Docker Hub.

---
+++ mvn-aggregate/build-and-push-all-images.sh
@@ -0,0 +1,7 @@
+mvn clean package -DskipTests
+docker push cosdin/07-micro-1:0.0.1-SNAPSHOT
+docker push cosdin/07-micro-2:0.0.1-SNAPSHOT
+docker push cosdin/07-config-server:0.0.1-SNAPSHOT
+docker push cosdin/netflix-eureka-naming-server:0.0.1-SNAPSHOT
+docker push cosdin/netflix-zuul-api-gateway-server:0.0.1-SNAPSHOT
+docker push cosdin/07-spring-admin-dev:0.0.1-SNAPSHOT
