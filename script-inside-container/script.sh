be149d320cbad16db9e975da00da3de198ded7ec
Cosmin Dinu <cosmin.dinu.ext@extendaretail.com>
2022-02-18 07:31:24 +0200

    Run script inside container. Call Elasticsearch from am3 dev server to delete all records. Modified ip from dev03rex.vrsspharma.com to 10.0.2.121

--- script-inside-container/script.sh
+++ script-inside-container/script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -x
-curl -X POST --header 'Authorization: Basic ZWxhc3RpYzpjaGFuZ2VtZQ==' --header "Content-Type: application/json" -d '{ "query": { "match_all": {} } }' 'http://dev03rex.vrsspharma.com:9200/logstash-2022.01.27-000001/_delete_by_query?conflicts=proceed'
+curl -X POST --header 'Authorization: Basic ZWxhc3RpYzpjaGFuZ2VtZQ==' --header "Content-Type: application/json" -d '{ "query": { "match_all": {} } }' 'http://10.0.2.121:9200/logstash-2022.01.27-000001/_delete_by_query?conflicts=proceed'
