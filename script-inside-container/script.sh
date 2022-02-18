1051e3f9473d5177217a4ab7fa083afc277ed36b
Cosmin Dinu <cosmin.dinu.ext@extendaretail.com>
2022-02-18 07:33:56 +0200

    Run script inside container. Call Elasticsearch from am3 dev server to delete all records. Run in while loop.

--- script-inside-container/script.sh
+++ script-inside-container/script.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 set -x
-curl -X POST --header 'Authorization: Basic ZWxhc3RpYzpjaGFuZ2VtZQ==' --header "Content-Type: application/json" -d '{ "query": { "match_all": {} } }' 'http://10.0.2.121:9200/logstash-2022.01.27-000001/_delete_by_query?conflicts=proceed'
+while $1
+do
+    echo "Run script inside container. Call Elasticsearch from am3 dev server to delete all records.."
+    sleep 20
+	curl -X POST --header 'Authorization: Basic ZWxhc3RpYzpjaGFuZ2VtZQ==' --header "Content-Type: application/json" -d '{ "query": { "match_all": {} } }' 'http://10.0.2.121:9200/logstash-2022.01.27-000001/_delete_by_query?conflicts=proceed'
+done
