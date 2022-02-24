88ec34330775ca8fdc3c7f24a4e821f4495e6fcf
Cosmin Dinu <cosmin.dinu.ext@extendaretail.com>
2022-02-24 17:43:25 +0200

    Run script inside container. Call Elasticsearch from am3 dev server to delete all records older than 5 days (was 2 days before this commit). Run in while loop.

--- script-inside-container/script.sh
+++ script-inside-container/script.sh
@@ -4,5 +4,5 @@ while $1
 do
     echo "Run script inside container. Call Elasticsearch from am3 dev server to delete records older than x days."
     sleep 20
-	curl -X POST --header 'Authorization: Basic ZWxhc3RpYzpjaGFuZ2VtZQ==' --header "Content-Type: application/json" -d '{"query": {"range": {"@timestamp": {"lte": "now-2d"}}}}' 'http://10.0.2.121:9200/logstash-2022.01.27-000001/_delete_by_query?conflicts=proceed'
+	curl -X POST --header 'Authorization: Basic ZWxhc3RpYzpjaGFuZ2VtZQ==' --header "Content-Type: application/json" -d '{"query": {"range": {"@timestamp": {"lte": "now-5d"}}}}' 'http://10.0.2.121:9200/logstash-2022.01.27-000001/_delete_by_query?conflicts=proceed'
 done
